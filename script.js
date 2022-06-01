c50f4e9f978479a3aa5ac7f0ca49923793811eaa
Azat-jyrgalbek-uulu <azat.jyrgalbekuulu.alt@gmail.com>
2022-06-01 23:47:59 +0600

    logic operators

--- script.js
+++ script.js
@@ -18,3 +18,46 @@ personalMovieDB.movies[quesionsA] = quesionsB;
 personalMovieDB.movies[quesionsC] = quesionsD;
 
 console.log(personalMovieDB);
+
+
+if (4 == 9) {
+    console.log("ok!")
+}else{
+    console.log('fuck you!!!')
+};
+
+(4 == 9) ? console.log('good job');
+
+
+console.log( 5 === 5 && 3 > 1 || 5);
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger || cola || fries === 3 || nuggets) {
+   console.log('Done!')
+}
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+
+
+
+
